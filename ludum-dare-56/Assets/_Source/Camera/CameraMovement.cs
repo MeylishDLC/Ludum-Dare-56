127f5698dd7dee23dc27e6fd2a555a663b7dbec9
MeylishDLC <meylishy20@gmail.com>
2024-10-05 11:56:07 +0300

    feat: basic camera movement

---
+++ ludum-dare-56/Assets/_Source/Camera/CameraMovement.cs
@@ -0,0 +1,96 @@
+using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Camera
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        private enum Positions
+        {
+            Middle,
+            Left,
+            Right
+        }
+        
+        [SerializeField] private float maxLeftPositionX;
+        [SerializeField] private float maxRightPositionX;
+        [SerializeField] private float edgePadding;
+        [SerializeField] private float moveDuration;
+
+        private Vector3 _initialPosition;
+        private Positions _currentPosition = Positions.Middle;
+        private bool _isMoving;
+        private void Start() 
+        {
+            _initialPosition = transform.position;
+        }
+        private void Update()
+        {
+            if (_isMoving)
+            {
+                return;
+            }
+            
+            var mousePos = Input.mousePosition;
+
+            if (mousePos.x <= edgePadding)
+            { 
+                //left
+                if (_currentPosition == Positions.Middle)
+                {
+                    MoveCameraAsync(Positions.Left).Forget();
+                }
+                if (_currentPosition == Positions.Right)
+                {
+                    MoveCameraAsync(Positions.Middle).Forget();
+                }
+            }
+            else if (mousePos.x >= Screen.width - edgePadding)
+            {
+                //right
+                if (_currentPosition == Positions.Middle)
+                {
+                    MoveCameraAsync(Positions.Right).Forget();
+                }
+                if (_currentPosition == Positions.Left)
+                {
+                    MoveCameraAsync(Positions.Middle).Forget();
+                }
+            }
+        }
+
+        private async UniTask MoveCameraAsync(Positions targetPosition)
+        {
+            if (_isMoving)
+            {
+                return;
+            }
+
+            _isMoving = true;
+            switch (targetPosition)
+            {
+                case Positions.Left:
+                    await transform.DOLocalMoveX(maxLeftPositionX, moveDuration).ToUniTask();
+                    _currentPosition = Positions.Left;
+                    break;
+                
+                case Positions.Middle:
+                    await transform.DOLocalMoveX(_initialPosition.x, moveDuration).ToUniTask();
+                    _currentPosition = Positions.Middle;
+                    break;
+                
+                case Positions.Right:
+                    await transform.DOLocalMoveX(maxRightPositionX, moveDuration).ToUniTask();
+                    _currentPosition = Positions.Right;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(targetPosition), targetPosition, null);
+            }
+
+            _isMoving = false;
+        }
+    }
+}
