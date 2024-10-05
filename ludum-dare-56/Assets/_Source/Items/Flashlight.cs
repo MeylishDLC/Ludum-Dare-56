4faa4ace6a04a37cd5f84c8e17fe826be03408c5
MeylishDLC <meylishy20@gmail.com>
2024-10-05 17:08:14 +0300

    feat: basic flashlight on hold

---
+++ ludum-dare-56/Assets/_Source/Items/Flashlight.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Items
+{
+    public class Flashlight: MonoBehaviour
+    {
+        [SerializeField] private GameObject lightObject;
+        
+        private void Start()
+        {
+            lightObject.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                var mousePos = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                var hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+                if (hit.collider != null)
+                {
+                    TurnOnFlashlight(true);
+                }
+                else
+                {
+                    TurnOnFlashlight(false);
+                }
+            }
+            else
+            {
+                TurnOnFlashlight(false);
+            }
+        }
+
+        public void TurnOnFlashlight(bool on)
+        {
+            lightObject.SetActive(on);
+        }
+    }
+}
