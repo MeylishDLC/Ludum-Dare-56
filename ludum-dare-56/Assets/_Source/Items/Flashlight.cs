4f5bb73fc8b9cf81e64084c0cb6ef31697d08966
MeylishDLC <meylishy20@gmail.com>
2024-10-06 09:52:02 +0300

    feat: flashlight charge

--- ludum-dare-56/Assets/_Source/Items/Flashlight.cs
+++ ludum-dare-56/Assets/_Source/Items/Flashlight.cs
@@ -1,19 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Items
 {
     public class Flashlight: MonoBehaviour
     {
-        public bool IsOn;
-        
+        public bool IsOn { get; private set; }
+
+        [SerializeField] private Image[] chargeBars;
         [SerializeField] private GameObject lightObject;
+        [SerializeField] private float chargeTime;
+
+        private float _remainingChargeTime;
+        private bool _canTurnOn = true;
+        
+        private int _currentBarIndex;
+        private float _timeToWasteOneBar;
+        private float _barTimeRemaining;
+        private CancellationTokenSource _cancelTrackBarsChargeCts = new();
         private void Start()
         {
+            SetBars();
             lightObject.gameObject.SetActive(false);
+            TrackFlashlightCharge(CancellationToken.None).Forget();
+            TrackBarsChange(_cancelTrackBarsChargeCts.Token).Forget();
         }
-
         private void Update()
         {
+            if (!_canTurnOn)
+            {
+                return;
+            }
+            
             if (Input.GetMouseButton(0))
             {
                 var mousePos = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -33,10 +53,63 @@ namespace Items
                 TurnOnFlashlight(false);
             }
         }
-        public void TurnOnFlashlight(bool on)
+        private void SetBars()
+        {
+            _currentBarIndex = chargeBars.Length - 1;
+            var barAmount = chargeBars.Length;
+            _timeToWasteOneBar = chargeTime / barAmount;
+        }
+        private void TurnOnFlashlight(bool on)
         {
             lightObject.SetActive(on);
             IsOn = on;
+        }
+        private async UniTask TrackFlashlightCharge(CancellationToken token)
+        {
+            _remainingChargeTime = chargeTime;
+            while (_remainingChargeTime > 0)
+            {
+                if (IsOn)
+                {
+                    _remainingChargeTime -= Time.deltaTime;
+                }
+                await UniTask.Yield(PlayerLoopTiming.TimeUpdate);
+            }
+            _canTurnOn = false;
+            TurnOnFlashlight(false);
+            if (_cancelTrackBarsChargeCts != null)
+            {
+                _cancelTrackBarsChargeCts.Cancel();
+                _cancelTrackBarsChargeCts.Dispose();
+                if (chargeBars[0].gameObject.activeSelf)
+                {
+                    chargeBars[0].gameObject.SetActive(false);
+                }
+            }
+        }
+        private async UniTask TrackBarsChange(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                _barTimeRemaining = _timeToWasteOneBar;
+                while (_barTimeRemaining > 0)
+                {
+                    if (IsOn)
+                    {
+                        _barTimeRemaining -= Time.deltaTime;
+                    }
+
+                    await UniTask.Yield(PlayerLoopTiming.Update);
+                }
+
+                chargeBars[_currentBarIndex].gameObject.SetActive(false);
+                _currentBarIndex--;
+                if (_currentBarIndex >= 0)
+                {
+                    continue;
+                }
+                break;
+            }
         }
     }
 }
